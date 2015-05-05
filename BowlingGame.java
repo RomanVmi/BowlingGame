2e955cfc6123566e48c79369b7fae1a0040c9159
Roman <romarick@xaker.ru>
2015-05-06 00:55:53 +0500

    FixSpareFail

--- BowlingGame.java
+++ BowlingGame.java
@@ -2,14 +2,23 @@
  * Created by Рома on 05.05.2015.
  */
 public class BowlingGame {
-
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < currentRoll; i++) {
+            if (rolls[i] + rolls[i+1] == 10) { //spare
+                score += 10 + rolls [i+2];
+                i++;
+            }   else {
+                score += rolls[i];
+            }
+        }
         return score;
     }
 }
