bdf9311b5d9e117b9d41ed2c8f0911eb8fdfe978
Roman <romarick@xaker.ru>
2015-05-05 23:57:38 +0500

    FirstTest

---
+++ BowlingGame.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Рома on 05.05.2015.
+ */
+public class BowlingGame {
+}
---
+++ BowlingGameTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Рома on 05.05.2015.
+ */
+public class BowlingGameTest {
+    @Test
+    public void CreateGame() {
+        BowlingGame game = new BowlingGame();
+
+    }
+}
